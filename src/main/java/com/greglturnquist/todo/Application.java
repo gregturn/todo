2fde994092cc477b14a51df43ef1413f809b3c54
Greg Turnquist <gturnquist@gopivotal.com>
2014-06-30 16:23:02 -0500

    Remove redundant annotation

--- src/main/java/com/greglturnquist/todo/Application.java
+++ src/main/java/com/greglturnquist/todo/Application.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @Configuration
 @ComponentScan
-@EnableJpaRepositories
 @Import(RepositoryRestMvcConfiguration.class)
 @EnableAutoConfiguration
 public class Application {
